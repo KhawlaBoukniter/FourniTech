0ffe4b648743e6c10a98fda096dbba9788794fe1
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-01 09:35:00 +0100

    define FournisseurServiceInterface with all required service methods

---
+++ src/main/java/org/fournitech_sb/service/interfaces/FournisseurServiceInterface.java
@@ -0,0 +1,19 @@
+package org.fournitech_sb.service.interfaces;
+
+
+import org.fournitech_sb.model.Fournisseur;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface FournisseurServiceInterface {
+    void addFournisseur(Fournisseur f);
+    void deleteFournisseur(Fournisseur f);
+    void updateFournisseur(Fournisseur f);
+    Fournisseur findFournisseurById(Long id);
+    Page<Fournisseur> findAllFournisseur(Integer page, Integer size, String sortBy, Boolean ascending);
+    Page<Fournisseur> findFournisseurByNom(String nom, Integer page, Integer size, String sortBy, Boolean ascending);
+    Page<Fournisseur> findFournisseurByNomEndingWith(String ending, Integer page, Integer size, String sortBy, Boolean ascending);
+    Long countFournisseurs();
+}
