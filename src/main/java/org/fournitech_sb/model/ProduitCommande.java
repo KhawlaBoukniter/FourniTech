6da29e803cbcd9cfda4453b80891c06db95136a8
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-03 15:10:00 +0100

    add ProduitCommande entity with produit, commande, quantite and prixUnit

---
+++ src/main/java/org/fournitech_sb/model/ProduitCommande.java
@@ -0,0 +1,31 @@
+package org.fournitech_sb.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Entity
+@Table(name = "produit_commande")
+@Data
+public class ProduitCommande {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "produit_id")
+    private Produit produit;
+
+    @ManyToOne
+    @JoinColumn(name = "commande_id")
+    @JsonManagedReference
+    private Commande commande;
+
+    private Integer quantite;
+
+    private Double prixUnit;
+}
