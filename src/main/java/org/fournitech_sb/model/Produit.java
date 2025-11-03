4635198b8c9f07a21c2c01009008b27b3df0de56
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-03 14:20:00 +0100

    add Produit entity with JPA annotations for database mapping

---
+++ src/main/java/org/fournitech_sb/model/Produit.java
@@ -0,0 +1,27 @@
+package org.fournitech_sb.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Entity
+@Table(name = "produits")
+@Data
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String description;
+
+    private Double prixUnit;
+
+    private String categorie;
+
+    private Integer stockActuel;
+}
