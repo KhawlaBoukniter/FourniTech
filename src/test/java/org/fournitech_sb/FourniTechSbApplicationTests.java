a8a22464e6ba5a89cc3108606930e1ef792f719d
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-04 08:30:00 +0100

    Initial project structure with packages and resources folders

---
+++ src/test/java/org/fournitech_sb/FourniTechSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fournitech_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourniTechSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
