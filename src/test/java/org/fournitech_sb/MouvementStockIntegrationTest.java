2486379d8c6df50dbf7997103242ba766aee7b18
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-14 16:40:00 +0100

    configure dynamic datasource properties for Testcontainers in MouvementStockIntegrationTest

---
+++ src/test/java/org/fournitech_sb/MouvementStockIntegrationTest.java
@@ -0,0 +1,49 @@
+package org.fournitech_sb;
+
+import org.fournitech_sb.dto.MouvementStockDto;
+import org.fournitech_sb.model.*;
+import org.fournitech_sb.repository.CommandeRepositoryInterface;
+import org.fournitech_sb.repository.MouvementStockRepositoryInterface;
+import org.fournitech_sb.repository.ProduitRepositoryInterface;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Testcontainers
+public class MouvementStockIntegrationTest {
+
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("fournitech_test")
+            .withUsername("test")
+            .withPassword("test")
+            .withReuse(true);
+
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+    }
+
+    
+
+}
